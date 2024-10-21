7eb5f6cb6ada78aeb7ef79695fa5c5a075e24701
tldoran <24418943@studentmail.ul.ie>
2024-10-21 05:41:15 -0500

    Added exception error

--- src/main/java/com/project/block1project/OperatingSystem.java
+++ src/main/java/com/project/block1project/OperatingSystem.java
@@ -1,16 +1,9 @@
-//just a random comment
 package com.project.block1project;
 
-//Imports Locale, Has a list of all country and language abbreviations
 import java.util.Locale;
 
-
-
-//New Class, OS
 public class OperatingSystem {
     public static void main(String[] args) {
-
-        //Get Operating system properties and save them as strings
         String OS = System.getProperty("os.name");
         String OS_bit = System.getProperty("sun.arch.data.model");
         String OS_Version = System.getProperty("os.version");
@@ -19,20 +12,18 @@ public class OperatingSystem {
         String LanguageAbbreviation = System.getProperty("user.language");
         String user = System.getProperty("user.name");
 
-        //Converts country code into country name
+        if (country == null || LanguageAbbreviation == null) {
+            System.err.println("Country or language system properties are not set.");
+            return;
+        }
+
         Locale locale = new Locale(LanguageAbbreviation, country);
         String CountryName = locale.getDisplayCountry();
         String language = locale.getDisplayLanguage();
 
-        //Formatting for OS type and build
-        System.out.printf("%s%s%s%s%s%n%s%s%n%s%s%n","OS = ", OS, " ", OS_bit, "-bit ","Version = ", OS_Version, "OS Architecture = ", OS_Architecture );
-
-        //Formatting fo country, language and user
+        System.out.printf("%s%s%s%s%s%n%s%s%n%s%s%n", "OS = ", OS, " ", OS_bit, "-bit ", "Version = ", OS_Version, "OS Architecture = ", OS_Architecture);
         System.out.printf("%s%s%n", "Country = ", CountryName);
         System.out.printf("%s%s%n", "System Language = ", language);
-        System.out.printf("%s%s%n",  "Current User = ", user);
-
-
-
+        System.out.printf("%s%s%n", "Current User = ", user);
     }
 }
