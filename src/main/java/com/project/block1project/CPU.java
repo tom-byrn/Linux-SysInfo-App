aa2ecd05064efcfde68488008bee92efb7dc41e9
bob <24403342@studentmail.ul.ie>
2024-10-22 14:26:30 +0100

    Trying to fix Mark's yolk

--- src/main/java/com/project/block1project/CPU.java
+++ src/main/java/com/project/block1project/CPU.java
@@ -1,7 +1,5 @@
 package com.project.block1project;
 
-import com.project.cs4421.project.cpuInfo;
-
 public class CPU {
 
     public static void main(String[] args){
--- src/main/java/com/project/block1project/IO_Devices.java
+++ src/main/java/com/project/block1project/IO_Devices.java
@@ -1,12 +1,13 @@
 package com.project.block1project;
 
+
 public class IO_Devices {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
 
 
 
 
     }
+
 }
