2c57f9ccdd68ca669e6461e7f2398a3c06af9102
bob <24403342@studentmail.ul.ie>
2024-10-24 11:39:49 +0100

    Added more info for OperatingSystem

--- src/main/java/com/project/block1project/PCIe.java
+++ src/main/java/com/project/block1project/PCIe.java
@@ -1,12 +1,34 @@
 package com.project.block1project;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+
 public class PCIe {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
 
 
 
+        //Checks if OS is linux
+        if (Objects.equals(System.getProperty("os.name"), "Linux")) {
+            // Execute the lspci command
+            Process LSPCI = Runtime.getRuntime().exec("lspci -vv");
+
+            // Read the output of the command
+            BufferedReader reader = new BufferedReader(new InputStreamReader(LSPCI.getInputStream()));
+            String line;
+
+            //prints out the PCI device info line by line
+            System.out.println("PCIe Devices:");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
 
 
-
+            }
+        }
     }
 }
